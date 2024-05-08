fa1fc34a7df74e5e64fe9c249fa31dff155c4d1c
Argeanu Georgiana <148890240+GeoArgeanu@users.noreply.github.com>
2024-05-08 18:01:04 +0300

    Add files via upload

---
+++ my_site/blog/views.py
@@ -0,0 +1,44 @@
+from urllib import request
+from xml.dom.minidom import Identified
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+from .forms import CommentForm
+
+
+# Create your views here.
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["comment_form"] = CommentForm()
+        return context
+
+
+    
+
+
